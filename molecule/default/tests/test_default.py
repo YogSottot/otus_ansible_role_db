a3a556f1a231592dc1790f38533efdba44553772
YogSottot <7411302+YogSottot@users.noreply.github.com>
2019-02-07 16:07:17 +0300

    Fix flake8 errors

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,9 +1,12 @@
 import os
 
+
 import testinfra.utils.ansible_runner
+
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
 
 # check if MongoDB is enabled and running
 def test_mongo_running_and_enabled(host):
@@ -11,11 +14,13 @@ def test_mongo_running_and_enabled(host):
     assert mongo.is_running
     assert mongo.is_enabled
 
+
 # check if configuration file contains the required line
 def test_config_file(host):
     config_file = host.file('/etc/mongod.conf')
     assert config_file.contains('bindIp: 0.0.0.0')
     assert config_file.is_file
+
 
 # check if MongoDB is listen on default port
 def test_mongo_listen_on_default_port(host):
